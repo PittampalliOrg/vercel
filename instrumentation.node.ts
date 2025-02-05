13f8fe82bab9da29b667baf67bc9ff6b8f58b5d1
Vinod Pittampalli <vinod@pittampalli.com>
2025-02-05 03:53:45 +0000

    feat: update OpenTelemetry configuration for enhanced logging and metrics support

--- instrumentation.node.ts
+++ instrumentation.node.ts
@@ -4,13 +4,37 @@ import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentation
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
 import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-proto';
 import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
+import { OTLPLogExporter } from '@opentelemetry/exporter-logs-otlp-proto';
+import { Resource } from '@opentelemetry/resources';
+import { ATTR_SERVICE_NAME, ATTR_SERVICE_VERSION } from '@opentelemetry/semantic-conventions';
+import { SimpleLogRecordProcessor } from '@opentelemetry/sdk-logs';
 
 const traceExporter =  new OTLPTraceExporter({
   url: 'http://otel-collector:4318/v1/traces'
-})
+});
+const metricReader = new PeriodicExportingMetricReader({
+  exporter: new OTLPMetricExporter({
+    url: 'http://otel-collector:4318/v1/metrics'
+  })
+});
+const logRecordExporter = new OTLPLogExporter({
+  url: 'http://otel-collector:4318/v1/logs'
+});
 
 const sdk = new opentelemetry.NodeSDK({
+  resource: new Resource({
+    [ATTR_SERVICE_NAME]: 'frontend',
+    [ATTR_SERVICE_VERSION]: '1.0.0'
+  }),
   traceExporter: traceExporter,
-  instrumentations: [getNodeAutoInstrumentations()],
+  metricReader: metricReader,
+  logRecordProcessors: [new SimpleLogRecordProcessor(logRecordExporter)],
+  instrumentations: [getNodeAutoInstrumentations({
+    "@opentelemetry/instrumentation-fs": {
+      enabled: false
+    }
+  })]
 });
+
 sdk.start();
+
